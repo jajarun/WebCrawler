6e7d06a65fa27b796e4514021f69fabb0b841064
majh <majiahao@yiwei.com>
2025-08-26 18:41:51 +0800

    add async crawler

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
+import asyncio
 import sys
 from src.services.crawler import Crawler
+from src.services.async_crawler import AsyncCrawler
         
-def main():
+async def main():
     #获取命令行第一个参数
     url = sys.argv[1] if len(sys.argv) > 1 else None
     if not url:
         print("请输入url")
         return
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        context = browser.new_context()
-        crawer = Crawler(context,url)
-        crawer.run()
-        browser.close()
+    
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context()
+        crawer = AsyncCrawler(context,url)
+        await crawer.arun()
+        await browser.close()
+
+    # with sync_playwright() as p:
+    #     browser = p.chromium.launch(headless=True)
+    #     context = browser.new_context()
+    #     crawer = Crawler(context,url)
+    #     crawer.run()
+    #     browser.close()
 
 # 示例用法
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
--- src/services/async_crawler.py
+++ src/services/async_crawler.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import BrowserContext, Page
+from playwright.async_api import BrowserContext, Page
 from typing import Optional,List,Set
 from bs4 import BeautifulSoup
 from src.utils.functions import deduplicate_text,check_main_domain
@@ -6,6 +6,7 @@ from dataclasses import field,dataclass,asdict
 import pandas as pd
 import time
 import re
+import asyncio
 
 @dataclass
 class CrawlerTable:
@@ -19,7 +20,7 @@ class webContent:
     title:str = ''
     contents:List[str] = field(default_factory=list)
 
-class Crawler:
+class AsyncCrawler:
 
     browserContext: Optional[BrowserContext] = None
 
@@ -35,12 +36,12 @@ class Crawler:
         self.browserContext = browserContext
         self.init_url = url
         self.table = CrawlerTable()
-        self.__clicked_elements = Set[str]()
-        self.__crawled_urls = Set[str]()
+        self.__clicked_elements = set[str]()
+        self.__crawled_urls = set[str]()
 
     #获取网页内容
-    def get_content(self,page:Page)->webContent:
-        page_content = page.content()
+    async def get_content(self,page:Page)->webContent:
+        page_content = await page.content()
         soup = BeautifulSoup(page_content, 'html.parser')
         content_list = soup.get_text(separator='<|>',strip=True).split('<|>')
         #文本去重
@@ -51,11 +52,11 @@ class Crawler:
         return webContent(title,contents=content_list)
     
     #获取所有链接
-    def get_link_urls(self,page:Page):
-        links = page.query_selector_all('a')
+    async def get_link_urls(self,page:Page):
+        links = await page.query_selector_all('a')
         linkUrls = []
         for link in links:
-            href = link.get_attribute('href')
+            href = await link.get_attribute('href')
             if href:
                 linkUrls.append(href)
         linkUrls = deduplicate_text(linkUrls)
@@ -67,19 +68,19 @@ class Crawler:
         return newLinkUrls
     
     #获取所有图片链接
-    def get_image_urls(self, page: Page):
-        images = page.query_selector_all('img')
+    async def get_image_urls(self, page: Page):
+        images = await page.query_selector_all('img')
         imageUrls = []
         for image in images:
-            src = image.get_attribute('src')
+            src = await image.get_attribute('src')
             if src:
                 # 过滤掉包含特定关键词的图片
                 if any(keyword in src for keyword in ["background", "icon", "sprite"]):
                     continue
                 
                 # 获取图片的宽度和高度
-                width = image.get_attribute('width')
-                height = image.get_attribute('height')
+                width = await image.get_attribute('width')
+                height = await image.get_attribute('height')
                 
                 # 过滤掉尺寸非常小的图片
                 if width and height and (int(width) < 50 or int(height) < 50):
@@ -91,59 +92,33 @@ class Crawler:
         return imageUrls
     
     #爬取子链接页面
-    def crawSubLinkPage(self,page:Page):
-        linkUrls = self.get_link_urls(page)
+    async def crawSubLinkPage(self,page:Page):
+        linkUrls = await self.get_link_urls(page)
+        tasks = []
         for linkUrl in linkUrls:
-            new_page = self.browserContext.new_page()
-            new_page.goto(linkUrl)
-            new_page.wait_for_load_state('networkidle')
-            self.craw_page(linkUrl,new_page)
-            self.__crawled_urls.add(linkUrl)
-            new_page.close()
+            tasks.append(self.craw_single_page(linkUrl))
+        #并行获取
+        await asyncio.gather(*tasks)
+
+    async def craw_single_page(self, linkUrl: str):
+        new_page = await self.browserContext.new_page()
+        await new_page.goto(linkUrl)
+        await new_page.wait_for_load_state('networkidle')
+        await self.craw_page(linkUrl, new_page)
+        self.__crawled_urls.add(linkUrl)
+        await new_page.close()
 
     #爬取点击元素页面
-    # def crawSubClickPage(self,page:Page):
-    #      # 查找并点击可点击的 <div> 元素
-    #     clickable_divs = page.query_selector_all('div[style*="cursor:pointer"], div.clickable')
-    #     #记录所有可点击div 供下次进入链接点击
-    #     for div in clickable_divs:
-    #         try:
-    #             div_outer_html = div.evaluate("(element) => element.outerHTML")
-    #             if div_outer_html in self.__clicked_elements:
-    #                 print("已经点击过:"+div_outer_html)
-    #                 continue
-    #         except Exception as e:
-    #             print("获取div元素失败:"+str(e))
-    #             continue
-    #         div.evaluate("(element) => element.click()")
-    #         self.__clicked_elements.add(div_outer_html)
-    #         # 等待页面加载完成
-    #         page.wait_for_load_state('networkidle')
-    #         #等待1秒
-    #         time.sleep(1)
-    #         # 获取当前页面url
-    #         current_url = page.evaluate("() => window.location.href")
-    #         if current_url not in self.__crawled_urls:
-    #             self.__crawled_urls.add(current_url)
-    #             self.craw_page(current_url,page)
-    #         else:
-    #             print("已经抓取过:"+current_url)
-    #         page.goto(self.init_url)
-    #         page.wait_for_load_state('networkidle')
-    #         self.crawSubClickPage(page)
-    #         break
-
-    #爬取点击元素页面
-    def crawSubClickPage(self, page: Page):
+    async def crawSubClickPage(self, page: Page):
         # 使用栈来模拟递归
         stack = [page]
         while stack:
             current_page = stack.pop()
-            clickable_divs = current_page.query_selector_all('div[style*="cursor:pointer"], div.clickable')
+            clickable_divs = await current_page.query_selector_all('div[style*="cursor:pointer"], div.clickable')
 
             for div in clickable_divs:
                 try:
-                    div_outer_html = div.evaluate("(element) => element.outerHTML")
+                    div_outer_html = await div.evaluate("(element) => element.outerHTML")
                     if div_outer_html in self.__clicked_elements:
                         print("已经点击过:" + div_outer_html)
                         continue
@@ -151,22 +126,22 @@ class Crawler:
                     print("获取div元素失败:" + str(e))
                     continue
 
-                div.evaluate("(element) => element.click()")
+                await div.evaluate("(element) => element.click()")
                 self.__clicked_elements.add(div_outer_html)
-                current_page.wait_for_load_state('networkidle')
+                await current_page.wait_for_load_state('networkidle')
                 time.sleep(1)
 
-                current_url = current_page.evaluate("() => window.location.href")
+                current_url = await current_page.evaluate("() => window.location.href")
                 if current_url not in self.__crawled_urls:
                     self.__crawled_urls.add(current_url)
-                    self.craw_page(current_url, current_page)
+                    await self.craw_page(current_url, current_page)
                 else:
                     print("已经抓取过:" + current_url)
 
                 # 将当前页面重新加入栈中以便继续处理
                 stack.append(current_page)
-                current_page.goto(self.init_url)
-                current_page.wait_for_load_state('networkidle')   
+                await current_page.goto(self.init_url)
+                await current_page.wait_for_load_state('networkidle')   
                 break         
 
     def add_table_row(self,webContent:webContent,url:str,images:List[str]):
@@ -179,30 +154,30 @@ class Crawler:
         df = pd.DataFrame(asdict(self.table))
         df.to_excel('output.xlsx', index=False)
 
-    def craw_page(self,url:str,page:Page):
+    async def craw_page(self,url:str,page:Page):
         print("开始爬取页面:",url)
         # 获取所有的button并点击
         if "faq" in url:
-            buttons = page.query_selector_all('button')
+            buttons = await page.query_selector_all('button')
             for button in buttons:
-                button.evaluate("(element) => element.click()")
+                await button.evaluate("(element) => element.click()")
         # 提取网页的所有文本内容
-        webContent = self.get_content(page)
+        webContent = await self.get_content(page)
         # 提取所有图片链接
-        imageUrls = self.get_image_urls(page)
+        imageUrls = await self.get_image_urls(page)
         # 添加表格行
         self.add_table_row(webContent,url,imageUrls)
 
-    def run(self):
-        page = self.browserContext.new_page()
+    async def arun(self):
+        page = await self.browserContext.new_page()
         # 打开目标网页
-        page.goto(self.init_url)
+        await page.goto(self.init_url)
         # 等待页面加载完成
-        page.wait_for_load_state('networkidle')
-        self.craw_page(self.init_url,page)
+        await page.wait_for_load_state('networkidle')
+        await self.craw_page(self.init_url,page)
         self.__crawled_urls.add(self.init_url)
         # 爬取子页面
-        self.crawSubLinkPage(page)
-        self.crawSubClickPage(page)
+        await self.crawSubLinkPage(page)
+        await self.crawSubClickPage(page)
         # 导出表格
         self.export_table()
